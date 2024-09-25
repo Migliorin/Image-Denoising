8e6dce1cd75fa6f852796071f8f41199a099a173
Lucas Migliorin da Rosa <lucasmigliorin@hotmail.com>
2024-09-25 12:57:34 +0000

    feat: New train using pytorch lighting save checkpoint and logs

---
+++ src/utils/light_module.py
@@ -0,0 +1,45 @@
+import lightning as L
+import torch
+
+
+
+class LightningVisionTransformer(L.LightningModule):
+    def __init__(self, model, loss, optim):
+        super().__init__()
+        self.model = model
+        self.loss = loss
+        self.optim = optim
+
+    def forward(self, denoised_img, original_img):
+        return self.model(denoised_img, original_img)
+
+    def training_step(self, batch, batch_idx):
+        ori_img, noi_img = batch
+
+        noi_img = noi_img.cuda()
+        denoised_img = self.model(noi_img)
+        denoised_img = denoised_img.cpu()
+
+        loss_ = self.loss(denoised_img, ori_img.cpu())
+
+        self.log("loss",loss_)
+
+        return loss_
+
+    def validation_step(self, batch, batch_idx):
+        self.model.eval()
+        ori_img, noi_img = batch
+
+        noi_img = noi_img.cuda()
+        denoised_img = self.model(noi_img)
+        denoised_img = denoised_img.cpu()
+
+        loss_ = self.loss(denoised_img, ori_img.cpu())
+
+        self.log("val_loss", loss_)
+
+        return loss_
+
+
+    def configure_optimizers(self):
+        return self.optim
