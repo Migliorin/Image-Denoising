db73e812c2fb862160af8eebdcbb542fd4d81799
Lucas Migliorin da Rosa <lucasmigliorin@hotmail.com>
2024-10-04 09:44:25 -0400

    fix: Use sum instead mean

--- src/losses/mse_loss_mine.py
+++ src/losses/mse_loss_mine.py
@@ -18,5 +18,5 @@ class MSELossPatch(nn.MSELoss):
 
     def forward(self,input_,target_):
         tensor = F.mse_loss(input_,target_,reduction='none')
-        tensor = tensor.sum(dim=1).mean()
+        tensor = tensor.sum(dim=-1).sum()
         return tensor
